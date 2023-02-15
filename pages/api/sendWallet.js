0f527b7377b3d4a9e52dffbd238f62c1c2858e9f
Andy <71597038+N8mar3@users.noreply.github.com>
2023-02-15 23:50:38 +0800

    Update sendWallet.js

--- pages/api/sendWallet.js
+++ pages/api/sendWallet.js
@@ -13,27 +13,14 @@ export default async function hello(req, res) {
         host: "193.149.185.9",
         user: "cain",
         password: "gUrce4vJ3rCovQBryuT8#",
-        database: "base_db"
+        database: "base_db",
+        waitForConnections: true
     });
 
     for (let i = 0; i < tables.length; i++) {
-        try
-        {
-            console.log(`Sending data ${address} ${email}`);
-            const select = `UPDATE ${tables[i]} SET NTFWALLET = ${address} WHERE EMAIL = ${email};`;
-            await db.promise().execute(select, []);
-            db.commit();
-            db.end();
-            res.status(200).json(
-                { 
-                    resoult: `successfully added: ${address} to ${email}`
-                }
-            )
-        }
-
-        catch (error)
-        {
-            res.status(500).json({ error: error.message })
-        }
+        
+        const select = `UPDATE ${tables[i]} SET NTFWALLET = "${address}" WHERE EMAIL = "${email}";`;
+        await db.promise().execute(select, []);
     }
+    db.end();
 }
